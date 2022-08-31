8acf5328a328ec5ff3d170c0e869f72b1078b291
Nathan Clonts <nacclonts@gmail.com>
2022-08-31 08:05:12 -0600

    increase z calculation depth; darken color; reduce margin

--- main.py
+++ main.py
@@ -11,15 +11,19 @@ import matplotlib.cm as cm
 # Pixel resolution
 im_width, im_height = 4500, 4500
 
-nit_max = 150
-c = complex(-1.3, 0.185)
-zabs_max = 150
+nit_max = 250
+c = complex(-1.25, 0.205)
+zabs_max = 250
 
 # Frame in cartesian space
 xmin, xmax = -1.9, 1.9
 xwidth = xmax - xmin
 ymin, ymax = -1.2, 1.2
 yheight = ymax - ymin
+
+# Color
+shade_multiplier = 0.1
+zshade_multiplier = 0.01
 
 # Percentage progress indicator
 progress_indicator_modulus = im_width / 10
@@ -48,7 +52,8 @@ for ix in range(im_width):
 		shade = nit / nit_max
 		zshade = (z / zabs_max).imag
 
-		julia[iy][ix] = np.array([shade*0.1 + zshade*0.1, shade*0.1 + zshade*0.1, shade*0.2 + zshade*0.1])
+		color_val = shade * shade_multiplier + zshade * zshade_multiplier
+		julia[iy][ix] = np.array([color_val, color_val, color_val])
 
   # print progress
 	if (ix > 0 and ix % progress_indicator_modulus == 0):
@@ -67,7 +72,9 @@ ax.set_yticks([(y-ymin) / yheight * im_height for y in ytick_labels])
 ax.set_yticklabels(['{:.1f}'.format(ytick) for ytick in ytick_labels])
 
 print('writing fractal...')
-plt.savefig('Green_jello_salad_%s_%si_%sj.png' % (
-	datetime.datetime.now(), c.imag, c.real,
-), dpi=1200)
+plt.savefig(
+	'Green_jello_salad_%s_%si_%sj.png' % (datetime.datetime.now(), c.imag, c.real),
+	dpi=1200,
+	bbox_inches='tight',
+)
 plt.show()
